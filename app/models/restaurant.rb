f9a0ee30db38da7a22fd7ee39ad9146928ccc426
Roberto Yáanez <reinier0211@gmail.com>
2021-07-23 18:07:29 -0400

    add yelp validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
   has_many :reviews
+  validates :name, :address, :category, presence: true
+  
 end
